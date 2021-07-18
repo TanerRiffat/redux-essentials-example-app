1bf7608ae0c4ac6e81bc7a2db3784eeba7f5eb24
TanerRiffat <taner.riffat@gmail.com>
2021-07-18 23:55:31 +1000

    first save to github

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import './index.css'
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
-
 import './api/server'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
