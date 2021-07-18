71ac23fed8b799769985d2d4ec91d71df5f33006
TanerRiffat <taner.riffat@gmail.com>
2021-07-19 00:30:35 +1000

    Updates

---
+++ precache-manifest.783d3301c7dc8849eeebb3ee2b7147e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19f3b29416431640500c70e706ade3a",
+    "url": "/redux-essentials-example/index.html"
+  },
+  {
+    "revision": "60e80cd70ab817015cff",
+    "url": "/redux-essentials-example/static/css/main.87992b5b.chunk.css"
+  },
+  {
+    "revision": "4268acaa673ffd4366c5",
+    "url": "/redux-essentials-example/static/js/2.ec337cb3.chunk.js"
+  },
+  {
+    "revision": "3f598473a645111fe9a1b1e43cd3e4b8",
+    "url": "/redux-essentials-example/static/js/2.ec337cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e80cd70ab817015cff",
+    "url": "/redux-essentials-example/static/js/main.fcb46a76.chunk.js"
+  },
+  {
+    "revision": "8f5bb3c35c6bc409a18c",
+    "url": "/redux-essentials-example/static/js/runtime-main.2766f08a.js"
+  }
+]);
